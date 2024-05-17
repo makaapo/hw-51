e2f182b717377c96216b56b66eb5c1b8f24ff07c
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-05-17 08:47:37 +0600

    create the initialization of the initial balls and a function that sorts the balls

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 
-const App = () => {
+const initialNumbers: number[] = [5, 11, 16, 23, 32];
 
+const generateNumbers = (): number[] => {
+    const numbers: number[] = [];
+    while (numbers.length < 5) {
+        const randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+        if (!numbers.includes(randomNumber)) {
+            const insertIndex = numbers.findIndex((num) => num > randomNumber);
+            if (insertIndex === -1) {
+                numbers.push(randomNumber);
+            } else {
+                numbers.splice(insertIndex, 0, randomNumber);
+            }
+        }
+    }
+    return numbers;
+};
+console.log(initialNumbers)
+console.log(generateNumbers())
 
-  return (
-    <>
-This is my app
-    </>
-  )
+const App: React.FC = () => {
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 };
 
-export default App
+export default App;
